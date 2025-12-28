27cb5db8281213928c6066a0fa355f6a519c99a3
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-28 23:31:40 +0100

    Moving script files to inline in playbook

--- ansible/roles/scripting-tasks/files/last-three-mod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATE=$(date +%Y-%m-%d)
-OUTPUT_FILE="/opt/scripts/output/last-three-mod-files-${DATE}.out"
-
-mkdir -p /opt/scripts/output
-
-ls -lt /var/log | grep "^-" | head -3 > "${OUTPUT_FILE}"
--- ansible/roles/scripting-tasks/files/loadavg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATE=$(date +%Y-%m-%d)
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-LOAD=$(cat /proc/loadavg | awk '{print $1, $2, $3}')
-
-echo "${TIMESTAMP} | Load: ${LOAD}" >> /var/log/loadavg-${DATE}.out
